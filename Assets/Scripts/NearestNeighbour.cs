79dfa95541882ea12f9e1478f7f74d3e04c6b29a
Kelvin Rosa <kelvinparkour@gmail.com>
2024-05-22 18:59:30 -0300

    Performance Improvements

--- Assets/Scripts/NearestNeighbour.cs
+++ Assets/Scripts/NearestNeighbour.cs
@@ -14,10 +14,8 @@ public class NearestNeighbour : MonoBehaviour
     private void Awake()
     {
         lineRenderer.positionCount = 2;
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
-        lineRenderer.startColor = Color.red;
-        lineRenderer.endColor = Color.red;
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.05f;
     }
 
     private void FixedUpdate()
